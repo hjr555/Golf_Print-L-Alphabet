c8013e8f0cf9ebc1964bd800ec05b7a73229c3a8
Hywel Rees <hjr555@gmail.com>
2016-08-12 10:53:52 +0100

    Initial ungolfed working version

--- Program.cs
+++ Program.cs
@@ -1,6 +1,35 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
+    char[] a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
     static void Main()
     {
+        int loop = 0;
+        int position = 0;
+        int letter = 65;
+
+        while(loop<26)
+        {
+            if(loop < position)
+            {
+                Console.Write((char)(position+65));
+            }
+            else
+            {
+                Console.Write((char)(letter + loop));
+            }
+
+            loop++;
+
+            if(loop == 26 && position < 25)
+            {
+                loop = 0;
+                position++;
+                letter = 65;
+                Console.WriteLine();
+            }
+        }
     }
 }
