e7ad865c5bbecf1ded30aee15705f7d8b9d3b4b4
Hywel Rees <hjr555@gmail.com>
2016-08-12 11:02:44 +0100

    Golfed - 178 bytes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿using System;
-
-class Program{
-    static void Main(){
-        int x,y,z=65;x=y=0;
-        while(x<26){
-            var j=x<y?(y+65):(z+x);
-            Console.Write((char)j);
-            if(++x==26&&y<25){x=0;z=65;y++;Console.WriteLine();}
-}}}
+﻿using System;class Program{static void Main(){int x,y,z=65;x=y=0;while(x<26){var j=x<y?(y+65):(z+x);Console.Write((char)j);if(++x==26&&y<25){x=0;z=65;y++;Console.WriteLine();}}}}
