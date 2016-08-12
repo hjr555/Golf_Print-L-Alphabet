197896e6c2009fac97fd7ea9d22c9151517118c4
Hywel Rees <hjr555@gmail.com>
2016-08-12 11:01:52 +0100

    Mostly minified

--- Program.cs
+++ Program.cs
@@ -1,26 +1,10 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
-    {
-        int x = 0;
-        int y = 0;
-        int z = 65;
-
-        while(x<26)
-        {
-            var j = x < y ? (y+65) : (z + x);
-
+class Program{
+    static void Main(){
+        int x,y,z=65;x=y=0;
+        while(x<26){
+            var j=x<y?(y+65):(z+x);
             Console.Write((char)j);
-
-            if(++x == 26 && y < 25)
-            {
-                x = 0;
-                z = 65;
-                y++;
-                Console.WriteLine();
-            }
-        }
-    }
-}
+            if(++x==26&&y<25){x=0;z=65;y++;Console.WriteLine();}
+}}}
